16e656b009e41070aa0ffc2f29cade29c4d79ca9
Murat Polat <bomch4nte@gmail.com>
2023-10-14 00:07:11 +0300

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { memo, useCallback, useState } from "react";
+import { GoogleMap, InfoWindow, useLoadScript, Marker } from "@react-google-maps/api";
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = "AIzaSyDAdnf6eqCFBTcFRQ3wHAFGIrVArQJzh0c";
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: apiKey,
+  });
+
+  const containerStyle = {
+    width: "100%",
+    height: "100vh",
+  };
+
+  const center = {
+    lat: 37.0012220691501,
+    lng: 35.32191010386118,
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isLoaded ? (
+        "Yükleniyor"
+      ) : (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={15}
+        >
+          {/* {visibleStops.map((stop) => (
+            <Marker
+              key={stop.stop_id}
+              position={{ lat: stop.stop_lat, lng: stop.stop_lon }}
+              onClick={() => handleMarkerClick(stop)}
+            />
+          ))}
+          {selectedStop && (
+            <InfoWindow
+              position={{
+                lat: selectedStop.stop_lat,
+                lng: selectedStop.stop_lon,
+              }}
+              onCloseClick={handleCloseInfoWindow}
+            >
+              <div>
+                <h3>Stop Name: {selectedStop.stop_name}</h3>
+                <p>Stop Description: {selectedStop.stop_desc}</p>
+                <p>Stop Code: {selectedStop.stop_code}</p>
+                <p>
+                  Stop Location: ({selectedStop.stop_lat},{" "}
+                  {selectedStop.stop_lon})
+                </p>
+              </div>
+            </InfoWindow>
+          )} */}
+        </GoogleMap>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
